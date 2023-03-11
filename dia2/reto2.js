cb0ca51ead2ff10961a29111e991859f7835c771
Celia Sánchez <celiasanchezmartin6@gmail.com>
2023-03-11 19:26:49 +0100

    retos3

---
+++ dia2/reto2.js
@@ -0,0 +1,19 @@
+//Reto 2.
+
+let object = {
+    name: "Celia",
+    surname: "Sánchez",
+    age: "25"
+}
+
+const fs = require("fs/promises");
+fs.writeFile("reto2.json", JSON.stringify(object))
+.then( () => {
+    return fs.readFile("reto2.json", "utf-8")
+})
+.then( data => {
+    console.log(JSON.parse(data));
+})
+.catch(err => {
+    console.log(err);
+});
---
+++ dia2/reto3.js
@@ -0,0 +1,49 @@
+//Reto 3.
+
+const fs = require("fs/promises");
+const readline = require("readline")
+
+function pregunta (pregunta) {
+    const question = new Promise((resolve, reject) => {
+        const rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
+        rl.question(pregunta, (respuesta) => {
+            resolve(respuesta);
+            rl.close();
+        })
+    })
+    return question;
+};
+
+let object = {
+    name: "Celia",
+    surname: "Sánchez",
+    age: 25
+};
+
+
+pregunta("¿Cómo te llamas?")
+.then((name) => {
+    object.name = name;
+    return pregunta("¿Cuál es tu apellido?")
+})
+.then((surname) => {
+    object.surname = surname;
+    return pregunta("¿Cuántos años tienes?")
+})
+.then((age) => {
+    object.age = age;
+    return fs.writeFile("reto3.json", JSON.stringify(object))
+
+})
+.then(() => {
+    return fs.readFile("reto3.json", "utf-8")
+})
+.then(data => {
+    console.log(JSON.parse(data) );
+})
+.catch(err => {
+    console.log(err);
+})
